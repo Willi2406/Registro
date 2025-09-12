85dcd2b600c1567a0209d0ebcb72f9786595858e
William Alexander Rodriguez Valentin <william_rodriguez@ucne.edu.do>
2025-09-11 22:14:46 -0400

    version 3

---
+++ Services/PartidasServices.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Registro.DAL;
+using Registro.Models;
+
+namespace Registro.Services
+{
+    public class PartidasServices(IDbContextFactory<Contexto> dbContextFactory)
+    {
+        private readonly IDbContextFactory<Contexto> _factory = dbContextFactory;
+
+        public async Task<bool> Existe(int id)
+        {
+            using var ctz = await _factory.CreateDbContextAsync();
+            return await ctz.Partidas.AnyAsync(e => e.JugadorId == id);
+        }
+
+        public async Task<bool> Guardar(Partidas partidas) => await 
+
+       
+
+    }
+}
